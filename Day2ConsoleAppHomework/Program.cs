4aea097f938d17b8152675da74b5da84a6cb0288
Birgitte Hvid <BIM@simcorp.com>
2022-08-29 19:54:33 +0200

    Assignment 1 tasks done.

--- Day2ConsoleAppHomework/Program.cs
+++ Day2ConsoleAppHomework/Program.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace Day2ConsoleAppHomework
 {
+    enum TransactionType { Buy, Sell }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Input the nominal of the trade:");
+            string userInput = Console.ReadLine();
+            var nominal = float.Parse(userInput);
+
+            Console.WriteLine("Input the trade price:");
+            userInput = Console.ReadLine();
+            var tradePrice = float.Parse(userInput);
+
+            Console.WriteLine("Input the transaction type:");
+            userInput = Console.ReadLine();
+            var transactionType = (TransactionType) Enum.Parse(typeof(TransactionType), userInput, true);
+
+            var sign=transactionType==TransactionType.Sell?-1:1;
+            var costValue = sign*nominal*tradePrice;
+
+            Console.WriteLine($"The calculated cost value is: {costValue}");
+            Console.ReadKey();
         }
     }
 }
