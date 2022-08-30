e0a8a9f012806be87374c7820c4b7a95c639c94a
Birgitte Hvid <BIM@simcorp.com>
2022-08-30 13:18:03 +0200

    Changes based upon feedback.

--- Day2ConsoleAppHomework/Program.cs
+++ Day2ConsoleAppHomework/Program.cs
@@ -6,38 +6,33 @@ namespace Day2ConsoleAppHomework
 
     class Program
     {
-        string currency = "DKK";
-
         static void Main(string[] args)
         {
-            string currency = "DKK";
+            const string currency = "DKK";
 
             Console.WriteLine("Input the transaction type:");
             var userInput = Console.ReadLine();
             var transactionType = (TransactionType)Enum.Parse(typeof(TransactionType), userInput, true);
 
             Console.WriteLine("Input the nominal of the trade:");
-            userInput = Console.ReadLine();
-            var nominal = float.Parse(userInput);
+            var nominal = float.Parse(Console.ReadLine());
 
             Console.WriteLine("Input the trade price:");
-            userInput = Console.ReadLine();
-            var tradePrice = float.Parse(userInput);
-            
+            var tradePrice = float.Parse(Console.ReadLine());
+
             Console.WriteLine("Input the trade price for the original trade (only used for sales):");
-            userInput = Console.ReadLine();
-            var origPrice = float.Parse(userInput);
-            
-            var sign=transactionType==TransactionType.Sell?-1:1;
-            var costValue = sign*nominal*tradePrice;
+            var originalPrice = float.Parse(Console.ReadLine());
+
+            var sign = transactionType == TransactionType.Sell ? -1 : 1;
+            var costValue = sign * nominal * tradePrice;
 
             var factor = transactionType == TransactionType.Sell ? 1 : 0;
-            var prl = factor * (tradePrice - origPrice) * nominal;
+            var profitLoss = factor * (tradePrice - originalPrice) * nominal;
 
-            var msg1 = "The calculated cost value is: {0:N2} {1}";
-            var msg2 = transactionType == TransactionType.Sell ? (", and the profit/loss is: {2:N2} {3}") : "";
+            var msg1 = "The calculated cost value is: {0:N2} {2}";
+            var msg2 = transactionType == TransactionType.Sell ? (", and the profit/loss is: {1:N2} {2}") : "";
 
-            Console.WriteLine(msg1+msg2,costValue,currency,prl,currency);
+            Console.WriteLine(msg1 + msg2, costValue, profitLoss, currency);
             Console.ReadKey();
         }
     }
